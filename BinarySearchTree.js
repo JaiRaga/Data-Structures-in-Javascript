c4d607dbf82a14eafc945b21a03d2f0ad05d1499
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-27 11:39:21 +0530

    added find method

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -35,6 +35,22 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+
+    while (current) {
+      if (value === current.value) return true;
+      if (value > current.value) {
+        if (current.right === null) return false;
+        current = current.right;
+      } else if (value < current.value) {
+        if (current.left === null) return false;
+        current = current.left;
+      }
+    }
+  }
 }
 
 const bst = new BinarySearchTree();
@@ -49,4 +65,5 @@ bst.insert(25);
 bst.insert(13);
 bst.insert(3);
 bst.insert(7);
+console.log(bst.find(21));
 console.log(bst);
