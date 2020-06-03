eb8d78b2a49d038d92bc7694c7113c925dd5c486
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-03 20:27:00 +0530

    added insert method

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -2,4 +2,34 @@ class MaxBinaryHeap {
   constructor() {
     this.values = [];
   }
+
+  insert(value) {
+    this.values.push(value);
+    this.bubbleUp();
+    return this;
+  }
+
+  bubbleUp() {
+    let index = this.values.length - 1,
+      parentInd = null,
+      temp = 0;
+
+    while (true) {
+      parentInd = Math.floor((index - 1) / 2);
+      console.log(index, parentInd);
+      if (this.values[index] <= this.values[parentInd]) break;
+      temp = this.values[parentInd];
+      this.values[parentInd] = this.values[index];
+      this.values[index] = temp;
+      index = parentInd;
+    }
+  }
 }
+
+let heap = new MaxBinaryHeap();
+console.log(heap.insert(41));
+// console.log(heap.insert(39));
+// console.log(heap.insert(33));
+// console.log(heap.insert(18));
+// console.log(heap.insert(27));
+// console.log(heap.insert(12));
