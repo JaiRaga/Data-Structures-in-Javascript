0fb28e4fb65757d53a2f6057549f742e90d0de68
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-04 12:47:37 +0530

    refactored insert method

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -10,26 +10,32 @@ class MaxBinaryHeap {
   }
 
   bubbleUp() {
-    let index = this.values.length - 1,
+    let ind = this.values.length - 1,
       parentInd = null,
-      temp = 0;
+      child = null,
+      parent = null;
 
     while (true) {
-      parentInd = Math.floor((index - 1) / 2);
-      console.log(index, parentInd);
-      if (this.values[index] <= this.values[parentInd]) break;
-      temp = this.values[parentInd];
-      this.values[parentInd] = this.values[index];
-      this.values[index] = temp;
-      index = parentInd;
+      if (ind === 0) break;
+      parentInd = Math.floor((ind - 1) / 2);
+      child = this.values[ind];
+      parent = this.values[parentInd];
+
+      if (child <= parent) break;
+      this.values[parentInd] = child;
+      this.values[ind] = parent;
+      ind = parentInd;
     }
   }
 }
 
 let heap = new MaxBinaryHeap();
 console.log(heap.insert(41));
-// console.log(heap.insert(39));
-// console.log(heap.insert(33));
-// console.log(heap.insert(18));
-// console.log(heap.insert(27));
-// console.log(heap.insert(12));
+console.log(heap.insert(39));
+console.log(heap.insert(33));
+console.log(heap.insert(18));
+console.log(heap.insert(27));
+console.log(heap.insert(12));
+console.log(heap.insert(55));
+console.log(heap.insert(40));
+console.log(heap.insert(100));
