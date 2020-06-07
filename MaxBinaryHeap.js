1af2283381becbd6fa86c7f61875c440062afe1f
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-07 12:25:35 +0530

    added extractMax method

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -4,7 +4,7 @@ class MaxBinaryHeap {
   }
 
   insert(value) {
-    this.values.push(value);
+    this.values.indexOf(value) === -1 ? this.values.push(value) : null;
     this.bubbleUp();
     return this;
   }
@@ -27,6 +27,44 @@ class MaxBinaryHeap {
       ind = parentInd;
     }
   }
+
+  extractMax() {
+    let root = this.values[0],
+      len = this.values.length - 1;
+    this.values[0] = this.values[len];
+    this.values[len] = root;
+    this.values.pop();
+    this.values.length > 1 ? this.bubbleDown() : null;
+    return root;
+  }
+
+  bubbleDown() {
+    let ind = 0,
+      lInd = null,
+      rInd = null,
+      parent = null,
+      lChild = null,
+      rChild = null;
+
+    while (true) {
+      lInd = 2 * ind + 1;
+      rInd = 2 * ind + 2;
+
+      lInd > this.values.length - 1 ? null : (lChild = this.values[lInd]);
+      rInd > this.values.length - 1 ? null : (rChild = this.values[rInd]);
+      parent = this.values[ind];
+
+      if (lChild > rChild && lChild > parent) {
+        this.values[lInd] = parent;
+        this.values[ind] = lChild;
+        ind = lInd;
+      } else if (rChild > lChild && rChild > parent) {
+        this.values[rInd] = parent;
+        this.values[ind] = rChild;
+        ind = rInd;
+      } else break;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
@@ -39,3 +77,6 @@ console.log(heap.insert(12));
 console.log(heap.insert(55));
 console.log(heap.insert(40));
 console.log(heap.insert(100));
+console.log(heap.insert(39));
+console.log(heap.extractMax());
+console.log(heap);
